b6f7ad6ea3f9b638e3209cc34414bf378c6e77c7
Habibur137 <infernohabib@gmail.com>
2022-02-10 19:13:32 +0600

    event delegate system added

--- js/event_buble.js
+++ js/event_buble.js
@@ -2,6 +2,27 @@ document.getElementById('item').addEventListener('click', function (event){
     console.log('li clicked')
     event.stopPropagation()
 })
+// event buble only parent event
 document.getElementById('list-container').addEventListener('click', function (event){
     console.log('ul clicked')
+})
+
+const items = document.getElementsByClassName('item');
+console.log(items)
+for(const item of items){
+    item.addEventListener('click', function (event){
+        // item.parentNode.removeChild(item)
+        // item.style.display = 'none'
+        // event.target.parentNode.removeChild(event.target)
+    })
+}
+
+document.getElementById('add-button').addEventListener('click', function (){
+    const p = document.createElement('p');
+    p.innerText = 'new Item added'
+    document.getElementById('item-container').appendChild(p)
+})
+
+document.getElementById('item-container').addEventListener('click', function (event){
+    event.target.parentNode.removeChild(event.target)
 })
